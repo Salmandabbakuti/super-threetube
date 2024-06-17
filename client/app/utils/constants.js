12011e6ed1898e317a34df7f99e93c921856cbd5
Salman Dabbakuti <dabbakutisalman@gmail.com>
2024-06-17 11:12:47 +0530

    updated: sq deployment to meter

--- client/app/utils/constants.js
+++ client/app/utils/constants.js
@@ -1,3 +1,3 @@
 // specific to meter testnet
 export const contractAddress = "0xe04aa72b9fEa2219F300b3f8C3f9D1eafEfE382e";
-export const subqueryUrl = "https://api.subquery.network/sq/Salmandabbakuti/thirdtube-sepolia";
+export const subqueryUrl = "https://api.subquery.network/sq/Salmandabbakuti/thirdtube-meter";
