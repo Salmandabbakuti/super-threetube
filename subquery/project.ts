54a2b032dbc9d13b519a534b2e85002e9d5d049a
Salman Dabbakuti <dabbakutisalman@gmail.com>
2024-06-17 11:05:35 +0530

    updated: sq config to meter testnet

--- subquery/project.ts
+++ subquery/project.ts
@@ -7,9 +7,9 @@ import {
 const project: EthereumProject = {
   specVersion: "1.0.0",
   version: "0.0.1",
-  name: "meter-starter",
+  name: "thirdtube-subql",
   description:
-    "This project can be use as a starting point for developing your new meter SubQuery project",
+    "Subquery project to index and query data from the ThirdTube contract on the Meter",
   runner: {
     node: {
       name: "@subql/node-ethereum",
@@ -24,17 +24,17 @@ const project: EthereumProject = {
     file: "./schema.graphql"
   },
   network: {
-    chainId: "534351",
-    endpoint: ["https://sepolia-rpc.scroll.io"]
+    chainId: "83",
+    endpoint: ["https://rpctest.meter.io"]
   },
   dataSources: [
     {
       kind: EthereumDatasourceKind.Runtime,
-      startBlock: 4626748,
+      startBlock: 59835067,
       options: {
         // Must be a key of assets
         abi: "ThirdTube",
-        address: "0x62862E40B26281130B7a32dbF682ac56a0201f0A"
+        address: "0xe04aa72b9fEa2219F300b3f8C3f9D1eafEfE382e"
       },
       assets: new Map([["ThirdTube", { file: "./abis/ThirdTube.json" }]]),
       mapping: {
